a78c04d589d8534bdd1a9d9604cf796b15a1620b
DeqnBerberov <deqnberberov@gmail.com>
2019-04-02 19:29:37 +0300

    Added Database layer
    Script used for commands for CRD. Doesnt work. Will fix tomorrow.

---
+++ project/project/database layer/Connection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project.database_layer
+{
+    public static class Connection
+    {
+        //String used to establish connection with SQL_server
+        private static string connectionString = "Server=.\\SQLEXPRESS;Database=Project;Integrated Security=true";
+        //Method used for getting the connectiongString from above.
+        public static SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
---
+++ project/project/database layer/NoteData.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace project.database_layer
+{
+    class NoteData
+    {
+        //Method used for Displaying all notes in the DB
+        public List<Note> ShowAll()
+        {
+            var NoteList = new List<Note>();
+            using (var connection = ConnectionDB.GetConnection())
+            {
+                var command = new SqlCommand("SELECT * FROM NOTE", connection);
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var product = new Note(
+                            reader.GetInt32(0),
+                            reader.GetString(1),
+                            reader.GetString(2)
+                        );
+
+                        NoteList.Add(product);
+                    }
+
+                }
+                connection.Close();
+
+                return NoteList;
+            }
+        }
+        //Method used for creating new note.
+        public void MakeNewNote(Note note)
+        {
+            using (var connection = ConnectionDB.GetConnection())
+            {
+                var command = new SqlCommand("SET IDENTITY_INSERT Note ON " +
+                "INSERT INTO Note(Id, Name, Description)" +
+                "VALUES(@id, @name, @description);", connection);
+
+                command.Parameters.AddWithValue("id", note.Id);
+                command.Parameters.AddWithValue("name", note.Name);
+                command.Parameters.AddWithValue("description", note.Description);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+
+        }
+
+        public Note ShowSpecificNote(int id)
+        {
+            Note note = null;
+            using (var connection = ConnectionDB.GetConnection())
+            {
+                var command = new SqlCommand("SELECT * FROM Note WHERE Id = @id", connection);
+                command.Parameters.AddWithValue("id", id);
+
+                connection.Open();
+                using (var reader = command.ExecuteReader())
+                {
+                    if (reader.Read())
+                    {
+                        note = new Note(
+                        reader.GetInt32(0),
+                        reader.GetString(1),
+                        reader.GetString(2)
+                        );
+                    }
+                }
+                connection.Close();
+            }
+
+            return note;
+        }
+
+        public void DeleteNote(int id)
+        {
+            using (var connection = ConnectionDB.GetConnection())
+            {
+                var command = new SqlCommand("DELETE note FROM Note where Id=@id;", connection);
+                command.Parameters.AddWithValue("id", id);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
+
+
+
