fa954e222e10254303d64355f6b09ec2144d7111
AlexandarMilissov <alex.milissov@gmail.com>
2019-04-03 19:18:14 +0300

    added register user interface

--- project/project/Models/User.cs
+++ project/project/Models/User.cs
@@ -4,10 +4,16 @@ using System.Text;
 
 namespace project.Models
 {
-    class User
+    public class User
     {
         public string Username { get; set; }
         public string Password { get; set; }
         public int UserId { get; set; }     
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
     }
 }
