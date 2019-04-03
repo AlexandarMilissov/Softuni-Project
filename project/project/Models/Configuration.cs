47a8410bcee8c0f29ce1ef293181d5317499d763
AlexandarMilissov <alex.milissov@gmail.com>
2019-04-03 20:52:31 +0300

    added select colour function

--- project/project/Models/Configuration.cs
+++ project/project/Models/Configuration.cs
@@ -4,10 +4,10 @@ using System.Text;
 
 namespace project.Models
 {
-    class Configuration
+    public class Configuration
     {
         public int Id;
-        public ConsoleColor textColour = ConsoleColor.White;
-        public ConsoleColor baclgroundColour = ConsoleColor.Black;
+        public ConsoleColor TextColour = ConsoleColor.White;
+        public ConsoleColor BackgroundColour = ConsoleColor.Black;
     }
 }
