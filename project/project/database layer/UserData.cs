a8ea13c9a14b608199d372bcb64555212674b0fe
DeqnBerberov <deqnberberov@gmail.com>
2019-04-03 19:21:26 +0300

    Added RegisterUser to User
    Register user makes new user

---
+++ project/project/database layer/UserData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using project.Models;
+
+
+namespace project.database_layer
+{
+    class UserData
+    {
+        public void RegisterUser(User user)
+        {
+            using (var connection = Connection.GetConnection())
+            {
+                var command = new SqlCommand("INSERT INTO Users(Username, Password)" +
+                "VALUES(@username, @password);", connection);
+                command.Parameters.AddWithValue("username", user.Username);
+                command.Parameters.AddWithValue("password", user.Password);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
