79c947a7be4c4678821ef08cfeed8e518f5d8cfa
DeqnBerberov <35681800+DeqnBerberov@users.noreply.github.com>
2019-04-04 20:20:35 +0300

    Updated Connection class
    Added xml description for this class

--- project/project/database layer/Connection.cs
+++ project/project/database layer/Connection.cs
@@ -7,9 +7,14 @@ namespace project.database_layer
 {
     public static class Connection
     {
-        //String used to establish connection with SQL_server
+        /// <summary>
+        /// This class is used for establishing connection with the SQL server
+        /// </summary>
         private static string connectionString = "Server=.\\SQLEXPRESS;Database=Project;Integrated Security=true";
-        //Method used for getting the connectiongString from above.
+        /// <summary>
+        /// Method used for getting the connection string from above. Looks more appropriate.
+        /// </summary>
+        /// <returns>Returns the connection string from above.</returns>
         public static SqlConnection GetConnection()
         {
             return new SqlConnection(connectionString);
