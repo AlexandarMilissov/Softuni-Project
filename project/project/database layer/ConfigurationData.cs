95c042ef62d45cac824ac04042bf3e580e282d54
DeqnBerberov <deqnberberov@gmail.com>
2019-04-04 22:50:22 +0300

    Added ConfigurationData class
    Added function to  add new configuration

---
+++ project/project/database layer/ConfigurationData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using project.Models;
+
+namespace project.database_layer
+{
+    class ConfigurationData
+    {
+        public void MakeNewConfiguration(Configuration configuration)
+        {
+            using (var connection = Connection.GetConnection())
+            {
+                var command = new SqlCommand("INSERT INTO Configurations(ConfigId, TextColor,BackgroundColor)" +
+                "VALUES(@configid, @textcolor,@bgcolor);", connection);
+
+                command.Parameters.AddWithValue("configid", configuration.Id);
+                command.Parameters.AddWithValue("textcolor", configuration.TextColour);
+                command.Parameters.AddWithValue("bgcolor", configuration.BackgroundColour);
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+    }
+}
